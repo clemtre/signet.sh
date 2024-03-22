8abd7f50d144468dcb5567b2cb125ce8169a7b4d
clemtre <martinlemairee@gmail.com>
2024-03-22 14:18:59 +0100

    v2 : no more dependy to jquery, signet generates <table> instead of <ol>

--- script.js
+++ script.js
@@ -1,59 +1,19 @@
-//document.addEventListener("keydown", function(event) {
-//    var keyPressed = event.key.toLowerCase();
-//
-//    if (keyPressed === 'p') {
-//        const val = Math.round(Math.random()*255)
-//        document.querySelector('body').style.background = `rgba(${val},${val /2},${val})`
-//        console.log("The 'P' key was pressed");
-//    }
-//});
+const textarea = document.querySelector('textarea')
 
-// Barre de recherche 
-$(document).ready(function () {
-    $('li').addClass('active')
-    $('textarea').on('input', function () {
-        var userInput = $(this).val().toLowerCase();
-
-        $('li').each(function () {
-            var listItemText = $(this).text().toLowerCase();
-            if (listItemText.includes(userInput)) {
-                $(this).addClass('active');
-            } else {
-                $(this).removeClass('active');
-            }
-        });
-
-    });
-});
-
-// On convertit les dates au format AA-MM-JJ
-function formatDateFromEpoch(epochTime) {
-    const date = new Date(epochTime * 1000); 
-
-    const YY = date.getFullYear().toString()
-    const MM = ('0' + (date.getMonth() + 1)).slice(-2)
-    const DD = ('0' + date.getDate()).slice(-2)
-
-    if(epochTime == ''){
-
-        return `N/A`;
-    }
-
-    return `${YY}/${MM}/${DD}`;
+function updateValue(e) {
+    let val = textarea.value.toLowerCase()
+    document.querySelectorAll('.signets tr').forEach((e) => {
+        var listItemText = e.innerHTML.toLowerCase();
+        listItemText.includes(val) ? 
+            e.classList.remove('hidden') : e.classList.add('hidden')        
+    })
 }
 
-const dates = document.querySelectorAll('h4')
-dates.forEach((date) => {
-    date.innerHTML= formatDateFromEpoch(date.innerHTML)
+document.addEventListener("DOMContentLoaded", function(event) {
+    textarea.addEventListener("input", updateValue);
 })
 
-// On cache les descriptions si elles sont vides
-$('h2').each(function() {
-    $(this).html() == '' ? $(this).hide() : $(this).show() 
-})
-
-// On colore les entrées qui ont un attribut 'color'
-$('[color]').each(function() {
-    $(this).css('background', `linear-gradient(var(--background), ${$(this).attr('color')} `)
+document.querySelectorAll('[color]').forEach((e) => {
+    e.style.background = `linear-gradient(var(--background), ${e.getAttribute('color')} `
 
 })
